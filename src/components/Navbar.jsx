a26ecfee9afdae6cda35d68aba5fb153dfdaca84
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-10-15 16:19:18 +0600

    update: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="py-4 max-w-5xl mx-auto">
       <div className="flex justify-between h-16 mx-auto">
@@ -32,31 +35,74 @@ function Navbar() {
               rel="noopener noreferrer"
               to="wishlist"
               className={({ isActive }) =>
-                isActive
-                  ? "text-violet-600 border-violet-600 flex items-center px-4 -mb-1 border-b-2"
-                  : "flex items-center px-4 -mb-1 border-b-2"
+                `flex items-center px-4 -mb-1 border-b-2 ${
+                  isActive ? "text-violet-600 border-violet-600" : ""
+                }`
               }
             >
               Wishlist
             </NavLink>
           </li>
         </ul>
-        <button className="flex justify-end p-4 md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="flex justify-end p-4 md:hidden"
+        >
+          {!isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z" />
+            </svg>
+          )}
         </button>
+      </div>
+      <div
+        className={`md:hidden w-full rounded-md overflow-hidden  ${
+          isOpen ? "animate-expand" : "animate-collapse"
+        }`}
+      >
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+          <li>
+            <NavLink
+              to="/"
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) =>
+                `block py-2 px-3 ${
+                  isActive ? "text-white bg-violet-600 rounded" : ""
+                }`
+              }
+              aria-current="page"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/wishlist"
+              onClick={() => setIsOpen(false)}
+              className={({ isActive }) =>
+                `block py-2 px-3 ${
+                  isActive ? "text-white bg-violet-600 rounded" : ""
+                }`
+              }
+            >
+              Wishlist
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </header>
   );
